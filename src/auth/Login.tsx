d4a555af8253bfa7133c905f40603b91136ed38f
Faiz Mohamad <153068612+mfaizmohamad@users.noreply.github.com>
2024-08-07 17:58:43 +0800

    login input

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -4,6 +4,7 @@ import routingPath from "../routing/Router_Path";
 import { errorListner, inputError } from "../utils/Input_error";
 import HeaderAuth from "../component/HeaderAuth";
 import { MdLockOutline } from "react-icons/md";
+import Footer from "../component/Footer";
 
 interface FormData {
   login: string;
@@ -110,13 +111,13 @@ const Login = () => {
                     required
                     error-required={"Please insert username"}
                     id="login"
-                    className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4"
+                    className="border rounded-l-lg border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-4"
                     placeholder="My Username"
                   />
                 </div>
                 <button
                   type="submit"
-                  className="h-[3.3rem] flex text-black bg-yellow-400 hover:bg-yellow-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                  className="h-[3.3rem] flex text-black bg-yellow-400 hover:bg-yellow-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-r-lg text-sm px-5 py-2.5 text-center"
                 >
                   <MdLockOutline className="mr-2 text-[1.5rem]" />
                   <span className="inline-block mt-1">LOGIN</span>
@@ -131,7 +132,7 @@ const Login = () => {
                 className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full flex"
               >
                 <div className="relative p-4 w-full max-w-md max-h-full">
-                  <div className="relative w-[50rem] h-[30rem] ml-[-10rem] opacity-95 bg-white rounded-lg shadow dark:bg-gray-700">
+                  <div className="relative w-[40rem] h-[20rem] ml-[-10rem] opacity-95 bg-white rounded-lg shadow dark:bg-gray-700">
                     <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                       <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
                         Please insert your password
@@ -160,7 +161,7 @@ const Login = () => {
                       </button>
                     </div>
                     <h1 className="text-center text-lg mt-10"><span className="font-bold">Hello</span> {loginUsername} !</h1>
-                    <div className="p-4 md:p-5 flex justify-center mt-[5rem]">
+                    <div className="p-4 md:p-5 flex justify-center mt-[1rem]">
                       <form
                         className="space-y-4 md:space-y-6"
                         onSubmit={handlePasswordSubmit}
@@ -179,12 +180,12 @@ const Login = () => {
                               error-required={"Please insert password"}
                               id="password"
                               placeholder="Password"
-                              className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3"
+                              className="border border-gray-300 text-gray-900 sm:text-sm rounded-l-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3"
                             />
                           </div>
                           <button
                             type="submit"
-                            className="h-[3rem] flex text-black bg-yellow-400 hover:bg-yellow-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                            className="h-[3rem] flex text-black bg-yellow-400 hover:bg-yellow-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-r-lg text-sm px-5 py-2.5 text-center"
                           >
                             <MdLockOutline className="mr-2 text-[1.5rem]" />
                             <span className="inline-block mt-1">LOGIN</span>
@@ -199,6 +200,7 @@ const Login = () => {
           </div>
         </div>
       </div>
+      <Footer/>
     </section>
   );
 };
